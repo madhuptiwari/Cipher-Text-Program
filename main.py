8433f7b40a8cc4dde6c4c8b0f95ddf4f6cb8a965
Madhup Tiwari <md.tiwari.9@gmail.com>
2024-07-19 12:06:33 +0530

    Cipher Code Update
    User can now input any number as shift amount for the message they ask the program to encode or decode

---
+++ main.py
@@ -0,0 +1,45 @@
+#List of alphabets using which the encode and decode find the position of the index and then shift the index by the key shared by the shift variable by the user. 
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Function caesar()) to encrypt and decrypt the message
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+    
+#Import log from art.py
+
+from art import logo
+print(logo)
+
+# Ask the user if they want to restart the cipher program
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+    
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+
+
+
